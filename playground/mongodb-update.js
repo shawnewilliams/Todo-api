2ddb5f5f35f1d5cfe3b2f5f7300529b04390c70d
Shawn Williams <shawn79williams@yahoo.com>
2018-03-14 16:13:01 -0400

    Add Update Script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,52 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // findOneAndUpdate()
+
+    db.collection('Users').findOneAndUpdate({
+        name: "Briana"
+    },{
+        $set: {
+            name: "Shawn"
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    })
+
+    // db.close();
+});
+
+// MongoDB module v3
+
+// MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+//     if(err) {
+//         return console.log('Unable to connect to MongoDB server');
+//     }
+//     console.log('Connected to MongoDB server');
+//     const db = client.db('TodoApp');
+
+//     db.collection('Todos').insertOne({
+//         text: 'Something to do',
+//         completed: false
+//     }, (err, result) => {
+//         if (err) {
+//             return console.log('Unable to insert todo', err);
+//         }
+
+//         console.log(JSON.stringify(result.ops, undefined, 2));
+//     });
+
+//     client.close();
+// });
