9f9186ad442bd0dd8e96e639fc5ed0c303f7284c
Shawn Williams <shawn79williams@yahoo.com>
2018-03-18 16:44:23 -0400

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
 
-var app = express();
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -53,6 +55,29 @@ app.delete('/todos/:id', (req, res) => {
     Todo.findByIdAndRemove(id).then((todo) => {
         if (!todo) {
             return res.status(404).send('Todo not found');
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('ID not valid');
+    }
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(400).send();
         }
         res.send({todo});
     }).catch((e) => {
